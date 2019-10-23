051711ed5ae9344122435ede208eda0ae24e9afb
Chengjian Wu <hardy0wu@gmail.com>
2019-10-23 13:28:13 +0800

    feat(seed): add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create email: 'admin@test.io', password: 'changeme'
+User.create email: 'user1@test.io', password: 'changeme'
+User.create email: 'user2@test.io', password: 'changeme'
+User.create email: 'user3@test.io', password: 'changeme'
+owner1 = Owner.create email: 'owner1@test.io', password: 'changeme'
+owner2 = Owner.create email: 'owner2@test.io', password: 'changeme'
+Owner.create email: 'owner3@test.io', password: 'changeme'
+
+owner1.stores.create name: 'store one'
+owner1.stores.create name: 'store two'
+owner1.stores.create name: 'store three'
+
+owner2.stores.create name: 'store 4'
+owner2.stores.create name: 'store 5'
